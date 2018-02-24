e15fadd961daada160dbbaf38dd23d0f4bc1653a
Dave <dkelly1818@yahoo.com>
2018-02-24 15:54:24 -0500

    Preliminary user code finished

--- lib/backend/User.java
+++ lib/backend/User.java
@@ -7,8 +7,36 @@ class User {
 	ArrayList<String> votes = new ArrayList<String>(25);
 	ArrayList<String> songsAdded = new ArrayList<String>(10);
 
-	public User() {
+	public User(String name) {
+		this.name = name;
+	}
 
+	public void addSong(Song song) {
+		Song.addSong(song);
+	}
+
+	public String removeSong(Song song) {
+		if(this.songsAdded.contains(song.URI)) {
+			Song.removeSong(song);
+			return "Song removed";
+		} else {
+			return "User did not add the song originally"
+		}
+	}
+
+	public void upvote(Song song) {
+		song.upvote();
+	}
+
+	public void downvote(Song song) {
+		song.downvote();
+	}
+
+	public boolean canUpvote(Song song) {
+//		if(this.songsAdded.contains(song.URI)) {
+//			return false;
+//		} else { return true; }
+		return this.songsAdded.contains(song.URI) ? false : true;
 	}
 
 }
