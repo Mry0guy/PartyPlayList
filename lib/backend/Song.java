894454e85f9252bbed54dca2ae2f1686da0a50b3
Dave <dkelly1818@yahoo.com>
2018-02-24 15:44:17 -0500

    More preliminary code

--- lib/backend/Song.java
+++ lib/backend/Song.java
@@ -1,7 +1,38 @@
-public class Song {
+package backend;
+class Song {
 
-	public Song() {
+	String name;
+	int score;
+	String URI;
+	String userID;
+	String URL;
 
+	public Song(String name, String URI, String userID, String URL) {
+		this.name = name;
+		this.score = 1;
+		this.URI = URI;
+		this.userId = userId;
+		this.URL = URL;
+	}
+
+	public void addSong(Song song) {
+
+	}
+
+	public void removeSong(Song song) {
+
+	}
+
+	public void upvote(Song song) {
+		this.song.score++;
+	}
+
+	public void downvote(Song song) {
+		this.song.score--;
+	}
+
+	public void getUpvotes(Song song) {
+		return this.song.score;
 	}
 
 }
---
+++ lib/backend/User.java
@@ -0,0 +1,14 @@
+package backend;
+import java.util.ArrayList;
+
+class User {
+	
+	String name;
+	ArrayList<String> votes = new ArrayList<String>(25);
+	ArrayList<String> songsAdded = new ArrayList<String>(10);
+
+	public User() {
+
+	}
+
+}
